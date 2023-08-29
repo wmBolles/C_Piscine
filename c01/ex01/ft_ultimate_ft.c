4be263cc2c1b4a9778bbbefc8be865bc4fce7801
cicbolles <108416484+cicbolles@users.noreply.github.com>
2023-08-29 16:28:58 +0100

    add ex00/ex01 in C01

---
+++ c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wabolles <wabolles@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 16:23:48 by wabolles          #+#    #+#             */
+/*   Updated: 2023/08/29 16:23:48 by wabolles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void    ft_ultimate_ft(int *********nbr)
+{
+    *********nbr = 0x2a;
+}
+
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int nb = 1337;
+
+    int *ptr1, **ptr2, ***ptr3, ****ptr4, *****ptr5, ******ptr6, *******ptr7, ********ptr8, *********ptr9;
+
+    ptr1 = &nb;
+    ptr2 = &ptr1;
+    ptr3 = &ptr2;
+    ptr4 = &ptr3;
+    ptr5 = &ptr4;
+    ptr6 = &ptr5;
+    ptr7 = &ptr6;
+    ptr8 = &ptr7;
+    ptr9 = &ptr8;
+    ft_ultimate_ft(ptr9);
+    printf("%d", nb);
+}
+*/
