0c3bf6840bc72caac2ab9a38cdcde5e7c7c3f1e7
wmbolles <wassimbolles18@gmail.com>
2023-08-30 15:05:09 +0100

    ft_putnbr

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -18,9 +18,10 @@ void    ft_putnbr(int nbr)
 {
     int     var;
 
-    var = ~(1 << ((sizeof(int) * 8) - 1));
-    var++;
-
+    var = ~(1 << ((sizeof(int) * 8) - 1)); //   step 1 : 1 = 000..0001,             this 1 of the sign bit !
+    var++;                                 //   step 2 : shift the 1 to the left : 1000...00000
+                                           //   step 3 : flip bits : 011111...11111 = max value of int.
+                                           // final step : var++ : round the value to the max negative value of int
     if (nbr == var)
     {
         write(1, "-2147483648", 11);
