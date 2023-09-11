bdf52960e40cfbecc6f07e71f456fc65eb9231b0
wmbolles <wassimbolles18@gmail.com>
2023-09-11 23:34:23 +0100

    FIXnorm

--- c01/ex05/ft_putstr.c
+++ c01/ex05/ft_putstr.c
@@ -6,18 +6,18 @@
 /*   By: wabolles <wabolles@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 19:29:47 by wabolles          #+#    #+#             */
-/*   Updated: 2023/08/29 19:29:47 by wabolles         ###   ########.fr       */
+/*   Updated: 2023/09/11 23:34:11 by wmbolles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-    while(*str)
-        write(1, str++, 1);
+	while (*str)
+		write(1, str++, 1);
 }
 
 ////////////////////////////////// for test ////////////////////////////////////
-int     main(void) { ft_putstr("Hello, World"); }
+//int     main(void) { ft_putstr("Hello, World"); }
 ////////////////////////////////// good luck ///////////////////////////////////
