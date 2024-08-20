fc738982c0567085aa1d1e034b38573599b3a1c0
TheF <wassimbolles18@gmail.com>
2024-08-20 10:36:18 +0100

    no_need_to_set_a_commit

---
+++ c05/ex04/ft_fibonacci.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wabolles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 11:14:10 by wabolles          #+#    #+#             */
+/*   Updated: 2023/07/25 11:26:41 by wabolles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_fibonacci(int index)
+{
+	if (index < 0)
+		return (-1);
+	if (index == 1 || index == 2)
+		return (1);
+	if (index == 0)
+		return (0);
+	return (ft_fibonacci(index - 2) + ft_fibonacci(index - 1));
+}
