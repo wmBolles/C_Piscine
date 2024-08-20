913fcc2e8bdfdd9aedf89cff6165d6629ba89508
TheF <wassimbolles18@gmail.com>
2024-08-20 10:34:28 +0100

    adding 03

---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wabolles <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/16 15:42:31 by wabolles          #+#    #+#             */
+/*   Updated: 2023/07/16 17:29:37 by wabolles         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0x0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+	unsigned int	j;
+
+	i = ft_strlen(dest);
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	if (size == 0 || size <= dest_len)
+		return (src_len + size);
+	j = 0x00000000;
+	while (src[j] && j < (size - dest_len - 1))
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
+}
